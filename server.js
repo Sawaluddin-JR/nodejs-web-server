87e25189385ffb10078b34f456cb09f5c89d4f1f
Sawaluddin <sawaluddinsrg472@gmail.com>
2024-02-24 20:29:22 +0700

    Menambahkan Body Request

--- server.js
+++ server.js
@@ -5,6 +5,28 @@ const http = require("http");
 const requestListener = (request, response) => {
   response.setHeader("Content-Type", "text/html");
   response.statusCode = 200;
+
+  const method = request.method;
+
+  if (method === "GET") {
+    response.end("<h1>Hallo</h1>");
+  }
+
+  if (method === "POST") {
+    let body = [];
+
+    request.on("data", (chunk) => {
+      body.push(chunk);
+    });
+
+    request.on("end", () => {
+      body = Buffer.concat(body).toString();
+      const { name } = JSON.parse(body);
+      response.end(`<h1>Hai,${name}!</h1>`);
+    });
+
+    //response.end("<h1>Hai</h1>");
+  }
 };
 
 const server = http.createServer(requestListener);
